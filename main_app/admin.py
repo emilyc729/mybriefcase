b067e032a91fda4082a4780b5c06f46bf790a31c
Emily <echeun17@mail.ccsf.edu>
2019-09-13 15:54:26 -0700

    register all models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Portfolio, Project, Photo
 
-# Register your models here.
+# Register you models here
+admin.site.register(Portfolio)
+admin.site.register(Project)
+admin.site.register(Photo)
