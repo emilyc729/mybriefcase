1f4a89c2ca3eeb72596b03d92e7795fe37ae4462
Emily <echeun17@mail.ccsf.edu>
2019-09-13 14:25:14 -0700

    created models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Photo Model
+class Photo(models.Model):
+    url = models.CharField(max_length=200)  #photo url
+
+    def __str__(self):
+        return f'{self.url}'
+
+#Portfolio Model
+class Portfolio(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    profile_link = models.CharField(max_length=100) #like a LinkedIn or SocialMedia Link
+    github_link = models.CharField(max_length=100) #main github page that shows all repositories
+    about_me = models.TextField(max_length=800) #description about user: interests/hobbies/goals
+    portfolio_photo = models.OneToOneField(Photo, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} portfolio'
+    
+    def get_absolute_url(self): 
+        return reverse('portfolio', kwargs={'pk':self.id})
+
+#Project Model
+class Project(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=20)   
+    technologies = models.TextField(max_length=200) #list of technologies used
+    deployed_link = models.CharField(max_length=100) #where project is deployed 
+    project_link = models.CharField(max_length=100) #project's github link
+    description = models.TextField(max_length=300)  #dmodelsate completed project
+    date = models.DateField()
+    project_photo = models.OneToOneField(Photo, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f'{self.portfolio.user} Project: {self.id} {self.project}'
