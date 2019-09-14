eaa51cbd15169511329b73d3e671c60942e89bdf
Emily <echeun17@mail.ccsf.edu>
2019-09-13 20:35:44 -0700

    updated models

--- main_app/models.py
+++ main_app/models.py
@@ -10,13 +10,13 @@ class Portfolio(models.Model):
     profile_link = models.CharField(max_length=100) #like a LinkedIn or SocialMedia Link
     github_link = models.CharField(max_length=100) #main github page that shows all repositories
     about_me = models.TextField(max_length=800) #description about user: interests/hobbies/goals
-    
+    photo_url = models.CharField(max_length=50, default="")
 
     def __str__(self):
         return f'{self.id} portfolio'
     
     def get_absolute_url(self): 
-        return reverse('portfolio', kwargs={'pk':self.id})
+        return reverse('portfolio_detail', kwargs={'pk':self.id})
 
 #Project Model
 class Project(models.Model):
@@ -27,6 +27,7 @@ class Project(models.Model):
     project_link = models.CharField(max_length=100) #project's github link
     description = models.TextField(max_length=300)  #dmodelsate completed project
     date = models.DateField()
+    photo_url = models.CharField(max_length=50, default="")
 
     def __str__(self):
         return f'{self.portfolio.user} Project: {self.id} {self.project}'
