eafeaba2850fe51dcdfdb3484088610a9cd2fb3d
Emily <echeun17@mail.ccsf.edu>
2019-09-15 00:51:13 -0700

    add _photo for portfolio  works

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('portfolio/<int:pk>/', views.PortfolioDetail.as_view(), name='portfolio_detail'),
     path('portfolio/create/', views.PortfolioCreate.as_view(), name='portfolio_create'),
     path('portfolio/<int:pk>/update/', views.PortfolioUpdate.as_view(), name='portfolio_update'),
-    path('portfolio/<int:portfolio_id>/add_photo/', views.add_photo, name='add_photo'),
+    path('portfolio/<int:portfolio_id>/add_photo/', views.portfolio_add_photo, name='portfolio_add_photo'),
     path('accounts/signup', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -30,7 +30,7 @@ class PortfolioDetail(LoginRequiredMixin, DetailView):
 
 class PortfolioCreate(LoginRequiredMixin, CreateView):
     model = Portfolio
-    fields = ['profession', 'profile_link', 'github_link', 'about_me', 'photo_url']
+    fields = ['profession', 'profile_link', 'github_link', 'about_me']
     success_url = '/portfolio/'
 
     def form_valid(self, form):
@@ -43,7 +43,7 @@ class PortfolioUpdate(LoginRequiredMixin, UpdateView):
 
     
 @login_required
-def add_photo(request, portfolio_id):
+def portfolio_add_photo(request, portfolio_id):
     photo_file = request.FILES.get('photo-file', None)
     portfolio = Portfolio.objects.get(id=portfolio_id)
     print(portfolio)
@@ -57,8 +57,7 @@ def add_photo(request, portfolio_id):
             s3.upload_fileobj(photo_file, BUCKET, key)
             url = f'{S3_BASE_URL}{BUCKET}/{key}'
             portfolio.photo_url = url
-            print(f'photo url : {portfolio.photo_url}')
-            portfolio.update(update_fields=['photo_url'])
+            portfolio.save()
         except:
             print('An error occurred uploading file to s3')
     return redirect(portfolio)
