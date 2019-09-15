1199147aefbf68456d4059a71990db3ede8693ad
sonniakemmer <brownbugz@gmail.com>
2019-09-15 13:42:14 -0700

    adds ProjectCreate, ProjectUpdate, and ProjectDelete urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     path('portfolio/<int:portfolio_id>/unassoc_project/<int:project_id>/', views.unassoc_project, name='unassoc_project'),
     path('projects/', views.ProjectList.as_view(), name='projects_index'),
     path('projects/<int:pk>/', views.ProjectDetail.as_view(), name='projects_detail'),
-    
+    path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
+    path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name='projects_update'),
+    path('projects/<int:pk>/delete/', views.ProjectDelete.as_view(), name='projects_delete')
 ]
