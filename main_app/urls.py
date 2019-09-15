fa9e70cab5aed96f848ef85b0a6a3de74267f015
sonniakemmer <brownbugz@gmail.com>
2019-09-15 13:55:41 -0700

    takes out assoc and unassoc urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,10 +9,6 @@ urlpatterns = [
     path('portfolio/<int:pk>/', views.PortfolioDetail.as_view(), name='portfolio_detail'),
     path('portfolio/<int:portfolio_id>/add_photo/', views.add_photo, name='add_photo'),
     path('accounts/signup', views.signup, name='signup')
-    # associate a project with a portfolio 
-    path('portfolio/<int:portfolio_id>/assoc_project/<int:project_id>/', views.assoc_project, name='assoc_project'),
-    # unassociate a project and portfolio
-    path('portfolio/<int:portfolio_id>/unassoc_project/<int:project_id>/', views.unassoc_project, name='unassoc_project'),
     path('projects/', views.ProjectList.as_view(), name='projects_index'),
     path('projects/<int:pk>/', views.ProjectDetail.as_view(), name='projects_detail'),
     path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
