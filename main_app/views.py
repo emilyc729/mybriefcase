571c0153d1d1b9500f8cedb48349b4fe7f918c37
sonniakemmer <brownbugz@gmail.com>
2019-09-15 13:58:15 -0700

    takes out assoc and unassoc function and adds Project model

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import User, Portfolio, Photo
+from .models import User, Portfolio, Photo, Project
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView, ListView
 from django.contrib.auth import login
@@ -77,14 +77,6 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
-
-def assoc_project(request, portfolio_id, project_id):
-    Portfolio.objects.get(id=portfolio_id).projects.add(project_id)
-    return redirect('detail', portfolio_id=portfolio_id)
-
-def unassoc_project(request, portfolio_id, project_id):
-    Portfolio.objects.get(id=portfolio_id).projects.remove(project_id)
-    return redirect('detail', portfolio_id=portfolio_id)
 
 class ProjectList(ListView):
     model = Project 
