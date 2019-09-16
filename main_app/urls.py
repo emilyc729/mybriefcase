b9ccd5edf0588a925850c726cf4d837e083c0f94
Emily <echeun17@mail.ccsf.edu>
2019-09-16 11:45:54 -0700

    removed assoc paths, added project photo paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,16 +10,12 @@ urlpatterns = [
     path('portfolio/<int:pk>/update/', views.PortfolioUpdate.as_view(), name='portfolio_update'),
     path('portfolio/<int:portfolio_id>/add_photo/', views.portfolio_add_photo, name='portfolio_add_photo'),
     path('portfolio/<int:portfolio_id>/delete_photo/', views.portfolio_delete_photo, name='portfolio_delete_photo'),
-    # associate a project with a portfolio 
-    path('portfolio/<int:portfolio_id>/assoc_project/<int:project_id>/', views.assoc_project, name='assoc_project'),
-    # unassociate a project and portfolio
-    path('portfolio/<int:portfolio_id>/unassoc_project/<int:project_id>/', views.unassoc_project, name='unassoc_project'),
-    # need to test path 
     path('projects/', views.ProjectList.as_view(), name='projects_index'),
     path('projects/<int:pk>/', views.ProjectDetail.as_view(), name='projects_detail'),
     path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
     path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name='projects_update'),
     path('projects/<int:pk>/delete/', views.ProjectDelete.as_view(), name='projects_delete'),
+    path('projects/<int:projects_id>/add_photo/', views.projects_add_photo, name='projects_add_photo'),
+    path('projects/<int:projects_id>/delete_photo/', views.projects_delete_photo, name='projects_delete_photo'),
     path('accounts/signup', views.signup, name='signup')
-
 ]
